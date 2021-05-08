d4e726562a7eb3c7e30a53e40b58dc0d7fc73a54
Victor Lava <victor.lava@aboutyou.com>
2021-05-08 12:10:42 +0300

    Added gopher.go

---
+++ src/gopher.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+    fmt.Println(time.Now());
+    fmt.Println("  __      _")
+    fmt.Println("o'')}____//")
+    fmt.Println("`_/      )")
+    fmt.Println("(_(_/-(_/")
+}
